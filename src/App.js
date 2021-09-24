764a273cac7c325ed816524ecd7227df51f1b26b
roshan Dahal <roshandahal747@gmail.com>
2021-09-23 20:54:11 -0600

    Sidebar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Topbar from './components/Topbar/Topbar';
+import Topbar from './components/Topbar.js/Topbar';
+
 
 function App() {
   return (
     <div className="App">
+
       <Topbar />
     </div>
   );
---
+++ src/components/Topbar.js/Topbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Topbar.css";
+import {NotificationsNone, Language , Settings} from "@material-ui/icons";
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Dahaladmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+             <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+             <Language /> 
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+           <Settings /> 
+          </div>
+          <img src="https://media-exp1.licdn.com/dms/image/C5603AQGlJfPN0Wu88w/profile-displayphoto-shrink_200_200/0/1627602129072?e=1637798400&v=beta&t=Xmd6QGwBTzDo2AAzea2SoJjfSMlNlCTPR2BjJhCqM-s" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
