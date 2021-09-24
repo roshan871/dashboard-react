53bed93177be096e734e16b883b54abc6dd77c98
roshan Dahal <roshandahal747@gmail.com>
2021-09-24 13:01:58 -0600

    Chart and dummData component

---
+++ src/dummyData.js
@@ -0,0 +1,61 @@
+export const userData = [
+    {
+      name: 'Jan',
+      'Active User': 4000,
+      
+    },
+    {
+      name: 'Feb',
+      'Active User': 10000,
+    },
+    {
+        name: 'Mar',
+        'Active User': 2000,
+        
+      },
+      {
+        name: 'Apr',
+        'Active User': 1500,
+        
+      },
+      {
+        name: 'May',
+        'Active User': 2500,
+        
+      },
+      {
+        name: 'Jun',
+        'Active User': 4000,
+        
+      },
+      {
+        name: 'Jul',
+        'Active User': 3000,
+        
+      },
+      {
+        name: 'Aug',
+        'Active User': 2000,
+        
+      },
+      {
+        name: 'Sep',
+        'Active User': 2500,
+        
+      },
+      {
+        name: 'Oct',
+        'Active User': 3500,
+        
+      },
+      {
+        name: 'Nov',
+        'Active User': 1500,
+        
+      },
+      {
+        name: 'Dec',
+        'Active User': 3000,
+        
+      },
+  ];
